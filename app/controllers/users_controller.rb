62942346b00a972ecb046ee712bdaa13f2ec466c
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-07-07 22:10:55 +0000

    strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #保存に成功した場合、ここに記述した処理が実施される。
+    else
+      render :new
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
