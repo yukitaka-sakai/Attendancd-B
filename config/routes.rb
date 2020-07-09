bcbb404867e640888f17058db161ece2152f2b76
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-07-09 22:08:46 +0000

    sessions ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-
-  root 'static_pages#top'
-  get '/signup', to: 'users#new'
+  root   'static_pages#top'
+  get    '/signup', to: 'users#new'
+  
+  get    '/login', to: 'sessions/new'
+  post   '/login', to: 'sessions/create'
+  delete '/logout', to: 'sessions/destroy'
+ 
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
