cc4c288d2a0e53e20c917ba38df46d5ec4cad72a
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-07-10 11:28:12 +0000

    login_form作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  
   root   'static_pages#top'
   get    '/signup', to: 'users#new'
   
-  get    '/login', to: 'sessions/new'
-  post   '/login', to: 'sessions/create'
-  delete '/logout', to: 'sessions/destroy'
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
  
   resources :users
 
