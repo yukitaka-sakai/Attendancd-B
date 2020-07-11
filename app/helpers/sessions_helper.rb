4ebe75c587d967a9828905635591327c5ab6c754
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-07-11 04:15:25 +0000

    log_in/outレイアウト分け

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,9 +7,13 @@ module SessionsHelper
   
   
   #現在ログイン中のユーザーがいる場合、オブジェクトを返す。
-  def crrent_user
+  def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
+  end
+  
+  def logged_in?
+    !current_user.nil?
   end
 end
