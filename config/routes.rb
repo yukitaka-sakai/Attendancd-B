955960fee247225f1f9d1bd0709709b3aa2ee26e
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-07-12 16:17:59 +0000

    モーダル表示

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,4 @@ Rails.application.routes.draw do
       patch 'update_basic_info'
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
