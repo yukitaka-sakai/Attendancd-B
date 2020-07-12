b7d469c11f4d0e21c8e523da7055fef283b51ab1
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-07-12 09:25:14 +0000

    デフォルト言語_日本語化

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'jbuilder',     '~> 2.5'
 gem 'faker'
 gem 'will_paginate'
 gem 'bootstrap-will_paginate'
+gem 'rails-i18n'
 
 group :development, :test do
   gem 'sqlite3'
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module AttendanceSystemB
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
