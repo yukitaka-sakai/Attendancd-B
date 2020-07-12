f2c310d10521b03c549c1f0fcfd513b5793e4668
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-07-12 06:14:12 +0000

    フレンドリーフォワーディング機能追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,19 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to user
-       # ログイン後にユーザー情報ページにリダイレクトします。
+      # user を引数にすることでデフォルトのURLを設定している。
+      redirect_back_or user
+      # ログイン後にユーザー情報ページにリダイレクトします。
     else
+      # .nowをつけることで１画面だけflashが表示される。
       flash.now[:danger] = "認証に失敗しました。"
+      # newアクションへ飛ぶ。つまり、ログインページに戻る。
       render :new
     end
   end
   
   def destroy
-    #ログイン中の場合のみログアウト処理できる。
+    # ログイン中の場合のみログアウト処理できる。
     log_out if logged_in?
     flash[:success] = "ログアウトしました。"
     redirect_to root_url
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -47,4 +47,15 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+  
+  #記憶しているURLまたは、記憶していない場合デフォルトで設定したURLへリダイレクト
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  #アクセスしようとしたURLを記憶する。
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
