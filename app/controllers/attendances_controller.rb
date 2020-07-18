7f727c4f152a4940df8604b4501e3c1affaf4713
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-07-18 10:53:11 +0000

    退社登録

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,14 +1,21 @@
 class AttendancesController < ApplicationController
+  UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
   
   def update
     @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
-    
+  
     if @attendance.started_at.nil?
       if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
-        flash[:into] ="おはようございます"
+        flash[:into] = "おはようございます！"
       else
-        flash[:danger] = "勤怠登録に失敗しました。やり直してください。"
+        flash[:danger] = UPDATE_ERROR_MSG
+      end
+    elsif @attendance.finished_at.nil?
+      if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
+        flash[:info] = "お疲れ様でした。"
+      else
+        flash[:danger] = UPDATE_ERROR_MSG
       end
     end
     redirect_to @user
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,14 @@
 module AttendancesHelper
+  
+  def attendance_state(attendance)
+    if Date.current == attendance.worked_on
+      # 勤怠データが当日、かつ出社時間が存在しない場合は”出社”
+      return "出社" if attendance.started_at.nil?
+      # 勤怠データが当日、かつ出社時間が登録済み、かつ退社時間が存在しない場合に”退社”
+      return "退社" if attendance.started_at.present? && attendance.finished_at.nil?
+    end
+    # 勤怠データが当日ではない、または、当日だが出社時間も退社時間も登録済みの場合”false”
+    false
+  end
 end
+
