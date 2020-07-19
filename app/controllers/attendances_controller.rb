26475d7867a10cd9cd1a73ccae022b3623302464
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-07-19 03:10:44 +0000

    編集認可機能追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,7 @@
 class AttendancesController < ApplicationController
   before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
   
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
@@ -48,5 +49,15 @@ class AttendancesController < ApplicationController
   def attendances_params
     # paramsハッシュの中の:userがキーのハッシュの中の:attendancesがキーのハッシュの中のidがキー各カラム名がキーとなり値がバリューとなった
     params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
+  end
+  
+  # beforeフィルター
+  # 管理権限者、または、現在ログインしているユーザー本人でありば実行可能
+  def admin_or_correct_user
+    @user = User.find(params[:user_id]) if @user.blank?
+    unless current_user?(@user) || current_user.admin?
+      flash[:danger] = "編集権限がありません。"
+      redirect_to(root_url)
+    end
   end
 end
