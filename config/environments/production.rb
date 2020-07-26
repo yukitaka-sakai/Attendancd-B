9aab8108e7dceb4815fe5a0698b15cda6474d1dc
yukitaka-sakai <aku.kaya.kulkas.yukitaka.ski@gmail.com>
2020-07-26 06:48:07 +0000

    assets>true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = ture
-  config.assets.initialize_on_precompile = false
+  config.assets.compile = true
+  
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
